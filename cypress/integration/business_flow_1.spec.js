f50f1f54ff1be9eda9e3e526e389655f4b7370e9
rotem weiss <rotem.weiss@worthy.com>
2021-09-19 12:17:02 +0300

    Added business flows

--- cypress/integration/business_flow_1.spec.js
+++ cypress/integration/business_flow_1.spec.js
@@ -6,16 +6,20 @@ import {
     removeItemFromCart
 } from "../support/commands"
 
+const registerFields = "label + div ";
+const firstName = "Rotem";
+
 describe('Register as a user', () => {
     it('Register as a user', function () {
         searchStoreAndNavigate("st");
         cy.get('[href="/my-account/auth"]').click();
         cy.contains("Create").click();
-        cy.get("label + div ").eq(0).type("Rotem").as("First Name");
-        cy.get("label + div ").eq(1).type("Weiss").as("Last Name");
-        cy.get("label + div ").eq(2).type("rotemvy@game.com").as("Email");
-        cy.get("label + div ").eq(3).type("123123").as("Password");
+        cy.get(registerFields).eq(0).type(firstName).as("First Name");
+        cy.get(registerFields).eq(1).type("Weiss").as("Last Name");
+        cy.get(registerFields).eq(2).type("rotemvy+"+ Math.random() +"@game.com").as("Email");
+        cy.get(registerFields).eq(3).type("123123").as("Password");
         cy.get("button").contains("Sign Up").click();
-
+        cy.wait(3000);
+        cy.get('button#my-account-link').should('include.text', "Hello, " + firstName + "!");
     });
 })
--- cypress/integration/business_flow_2.spec.js
+++ cypress/integration/business_flow_2.spec.js
@@ -13,9 +13,13 @@ describe('Go to cart and checkout your items', () => {
         openCart();
         cy.wait(4000);
         cy.get('[href="/cart/checkout"]').click();
+        cy.wait(3000);
         cy.get('[data-testid="store.cart.deliveryDetails.aptUnit"]').type("3");
         cy.get('[data-testid="store.cart.deliveryDetails.phone"]').type("2343256457");
         cy.wait(4000);
-        cy.get("[placeholder='Card number']").type("2010 1494 7776 5720 0926 234");
+        cy.get("#card-element input").type("2010 1494 7776 5720 0926 234");
+        cy.get("[data-testid='store.placeOrderButton'").click();
+        //TODO add assertion cypress click the checkout button but wont print error massage
+        cy.get("p").contains("Your card number is incorrect.")
     });
 })
